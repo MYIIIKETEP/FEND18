5b97b9ba38fb0167416f326dbf62978cf7fd43d0
Denis Jakusjev <Denis.Jakusjev@gmail.com>
2018-10-21 21:27:49 +0200

    all done

--- Inlmnng3/script.js
+++ Inlmnng3/script.js
@@ -40,7 +40,7 @@ function uppgift3(){
     let pi3 = +Math.PI;
     pi3.toFixed(3);
     let omkrets = (rad*2) * pi3;
-    let area = (rad*rad) * pi3;
+    let area = Math.pow(rad,2) * pi3;
     let msg = "";
     msg += "Omkretsen är " + omkrets.toFixed(3);
     msg += '\n'
@@ -48,4 +48,21 @@ function uppgift3(){
     alert(msg);
     
 }
+
+
+function uppgift4 (){
+ let rad = prompt("Ange Radius på din sfär");
+ let pi3 = Math.PI;
+ pi3.toFixed(3);
+
+ let volym = ((4*pi3) * (Math.pow(rad,3))) / 3
+ let arean = (4*pi3) * Math.pow(rad,2);
+ let msg = "";
+ msg += "Omkretsen är " + volym.toFixed(3);
+ msg += '\n'
+ msg += "Arean är " + arean.toFixed(3);
+ alert(msg);
+
+}
+
 
