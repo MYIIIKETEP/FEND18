db7f95c66896c87b604a170b6214af05294f079c
Denis Jakusjev <Denis.Jakusjev@gmail.com>
2018-10-21 23:06:50 +0200

    Lektion 9 all usefull

---
+++ Lektion9JS/script.js
@@ -0,0 +1,18 @@
+let firstName = "Denis"
+firstName.length(); //Длинна слова
+
+let name1 = "Denis";
+let name2 = "Jakusjev";
+let fullname = name1.concat(' '+ name2); // Вместо плюса юзаем CONTACT
+
+//НЕПРАВИЛЬНО
+let myName = "Denis";
+myName.toUpperCase();
+//Правильно
+mynName = myName.toUpperCase();//DENIS
+myName = myName.toLowerCase(); //denis
+
+//Generera slumptal mellan 1 och 10
+console.log(Math.random() * 10 + 1);
+console.log(Math.random() * 10 + 1);
+console.log(Math.random() * 10 + 1);
