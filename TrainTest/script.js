45ea74d588a0fecc55b549abe145d485bac11d46
Denis Jakusjev <qwarick@hotmail.com>
2018-11-07 11:48:33 +0100

    BirthDate

---
+++ TrainTest/script.js
@@ -0,0 +1,16 @@
+
+while(true){
+let birthdate = promt("ÅÅÅÅ-MM-DD", "ÅÅÅÅ-MM-DD");
+console.log(birthdate);
+
+//Om användaren klickar på avbryt eller klickar på escape
+if(birthdate === null){
+    break;
+}
+}
+if(birthdate < 10){
+    alert
+}
+
+
+
