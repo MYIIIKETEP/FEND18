7c0c8dfd1f3ccb76b9efdce7dd98c6340a11431e
Denis Jakusjev <Denis.Jakusjev@gmail.com>
2019-03-21 22:23:48 +0100

    myOwn

---
+++ MyOwn/WebShopTry/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#ClickMe").click(function(){
+        $("#panel").slideToggle("slow");
+        //$("#panel").slideUp("slow"); Css on slide div NO Display:none;
+    });
+  });
